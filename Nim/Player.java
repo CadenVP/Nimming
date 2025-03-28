f26c83ddb4612dbf6b1be50d9a06c09d37042c46
CadenVP <cadenvp@gmail.com>
2025-03-28 09:45:45 -0700

    Add files via upload

--- Nim/Player.java
+++ Nim/Player.java
@@ -1,27 +1,64 @@
 import java.util.Scanner;
 public class Player {
-    private int score;
+    private static int s1;
+    private static int s2;
     private static int numPieces;
     private static String name;
-    private int order;
     private static int numPlayers;
     static Scanner s = new Scanner(System.in);
-                
     public static String makeName(){
         System.out.println("Whats your name");
         name = s.nextLine();
         return name;
     }
-    
-    public Player(String name, int order){
-        this.name = name;
-        this.order = order;
-    }
-    public String getName(){
-        return name;
-    }
-    public int getOrder(){
-        return order;
+    public static void pvp(String[] nList){
+        numPieces = (int)(Math.random()*40)+10;
+        System.out.println(numPlayers+" "+numPieces);
+        int x = (int)(Math.random()*2);
+        System.out.println(x);
+        System.out.println("You have "+numPieces+" pieces");
+        while(numPieces>1){
+            if(nList.length>1){
+                System.out.println("How many do you want to take away "+nList[x%2]+"?: ");
+            }
+            else{
+                System.out.println("How many do you want to take away "+nList[0]+"?: ");
+            }
+            int take = s.nextInt();
+            s.nextLine();
+            if(take < (numPieces/2)+1 && numPieces-take > 0 && take>0){
+                numPieces -= take;
+                x++;
+                System.out.println("You now have "+numPieces+" left");
+            }
+            else{
+                System.out.println("Bad input");
+            }
+            if(nList.length>1){
+                int cTake = (int)(Math.random()*numPieces);
+                numPieces -= cTake;
+                System.out.println("The computer took "+cTake+" pieces");
+                System.out.println("There are now "+numPieces+" left");
+            }
+        }
+        System.out.println(nList[(x-1)%2]+" wins!");
+        if(nList[(x-1)%2]==nList[0]){
+            s1++;
+        }
+        else{
+            s2++;
+        }
+        if(nList.length>1){
+            System.out.println(nList[0]+": "+s1+"\n"+nList[1]+": "+s2);
+        }
+        else{
+            System.out.println(nList[0]+": "+s1+"\ncomputer: "+s2);
+        }
+        System.out.println("Play again?(y/n): ");
+        String pa = s.nextLine();
+        if(pa.indexOf("y")!=-1){
+            pvp(nList);
+        }
     }
     public static void nimming(){
         System.out.println("How many players: ");
@@ -30,29 +67,17 @@ public class Player {
         if(numPlayers == 2){
             System.out.println("Player 1: ");
             String p1 = makeName();
-            
             System.out.println("Player 2: ");
             String p2 = makeName();
             String[] nList = {p1,p2};
             System.out.println(p1 +" "+p2);
-            numPieces = (int)(Math.random()*40)+10;
-            System.out.println(numPlayers+" "+numPieces);
-            int x = 0;
-            while(numPieces>1){
-                System.out.println("How many do you want to take away "+nList[x%2]+"?: ");
-                int take = s.nextInt();
-                if(take < (numPieces/2)+1 && numPieces-take > 0){
-                    numPieces = numPieces - take;
-                    x++;
-                    System.out.println("You now have "+numPieces+" pieces");
-                }
-                else{
-                    System.out.println("Bad input");
-                }
-            }
-            System.out.println(nList[(x-1)%2]+" wins!");
+            pvp(nList);
         }
         else if(numPlayers == 1){
+            System.out.println("Player 1: ");
+            String p1 = makeName();
+            String[] nList = {p1};
+            pvp(nList);
             System.out.println("cool this works");
         }
         else{
@@ -61,7 +86,6 @@ public class Player {
         }
         
     }
-
     public static void main(String[] args) {
         nimming();
     }
